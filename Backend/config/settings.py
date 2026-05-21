1ce26020bc51fafc3eceb2bc24af7abffabfd2cc
Harold Esteban Duran Osma <duranosmaharold@gmail.com>
2026-05-21 16:27:11 -0500

    fix:fix path versel

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'covagroback.vercel.app',
+    'covagroback-6ym84v45n-teban135s-projects.vercel.app',
     os.getenv('ALLOWED_HOST', 'localhost'),
 ]
 
