157a75b160a5292ec7012411fe68374da53890e8
Harold Esteban Duran Osma <duranosmaharold@gmail.com>
2026-05-21 10:29:49 -0500

    fix:requirements.txt

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '*.vercel.app',
+    os.getenv('ALLOWED_HOST', 'localhost'),
+]
 
 
 # Application definition
